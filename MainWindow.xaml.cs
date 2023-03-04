8252f32e76ee969fb4106f0240b9f5a9dbb9c828
Максим Шабанов <maksim20048.ru@gmail.com>
2023-03-04 15:31:00 +0400

    testing json creations and logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,15 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using workWithFiles;
 
 namespace cursach
 {
-    class Question
-    {
-        public Question() { }
-        string QuestionText;
-
-    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -29,6 +24,17 @@ namespace cursach
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            Test testobj = new Test("тестовый", new List<Question>() {
+            new Question("2 + 2 = ?",new List<string>{"3","4","5"},new List<string>{"4"}),
+            new Question("5 x 3 = ?",new List<string>{"12","13","15"},new List<string>{"15"}),
+            new Question("10 - 6 = ?",new List<string>{"2","3","4"},new List<string>{"4"}),
+            new Question("20 / 4 = ?",new List<string>{"4","5","6"},new List<string>{"5"}),
+            new Question("12 + 8 x 2 = ?",new List<string>{"28","32","40"},new List<string>{"28"})
+            });
+            WorkWithFiles.createJsonFromTestClass(testobj);
         }
     }
 }
---
+++ WorkWithFiles.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+
+namespace workWithFiles
+{
+    class Test
+    {
+        public string name;
+        public List<Question> questions;
+        public Test(string name, List<Question> questions)
+        {
+            this.name = name;
+            this.questions = questions;
+        }
+    }
+    class Question
+    {
+        public string questionName;
+        public List<string> answers;
+        public List<string> rightAnswers;
+        public Question(string questionName, List<string> answers, List<string> rightAnswers)
+        {
+            this.questionName = questionName;
+            this.answers = answers;
+            this.rightAnswers = rightAnswers;
+        }
+    }
+    class WorkWithFiles
+    {
+        public static void createJsonFromTestClass(Test testObj)
+        {
+            string json = JsonConvert.SerializeObject(testObj);
+            if (!File.Exists("D:\\amalgama.json"))
+                File.Create("D:\\amalgama.json");
+            File.WriteAllText("D:\\amalgama.json", json);
+        }
+
+    }
+}
