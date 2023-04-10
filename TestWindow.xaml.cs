a6b589858e7cfc6b57a5dea1d7d4893021b1e4bd
Максим Шабанов <maksim20048.ru@gmail.com>
2023-04-10 16:44:21 +0400

    сбор ответов

--- TestWindow.xaml.cs
+++ TestWindow.xaml.cs
@@ -1,41 +1,70 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace cursach
 {
-    public partial class TestWindow : Window
-    {
-        public TestWindow(Test testObj)
-        {
-            InitializeComponent();
-            QuestionsPanel.Children.Add(CreateVisualOfQuestions(testObj));
-        }
-        private StackPanel CreateVisualOfQuestions(Test test)
-        {
-            StackPanel questionsPanel = new StackPanel();
-            foreach (Question question in test.questions)
-            {
-                TextBlock questionName = new TextBlock();
-                questionName.Text = question.questionName;
-                questionName.FontSize = 15;
-                questionsPanel.Children.Add(questionName);
-                questionsPanel.Children.Add(CreateVisualOfOptions(question));
-            }
-            return questionsPanel;
-        }
-        private StackPanel CreateVisualOfOptions(Question question)
-        {
-            StackPanel optionsPanel = new StackPanel();
-            optionsPanel.Orientation = Orientation.Horizontal;
-            foreach (string option in question.options)
-            {
-                RadioButton answer = new RadioButton();
-                answer.Content = option;
-                answer.Margin = new System.Windows.Thickness(5);
-                optionsPanel.Children.Add(answer);
-            }
-            optionsPanel.Margin = new System.Windows.Thickness(10);
-            return optionsPanel;
-        }
-    }
+	public partial class TestWindow : Window
+	{
+		Dictionary<string, string> answers = new();
+		public TestWindow(Test testObj)
+		{
+			InitializeComponent();
+			TestName.Text = testObj.testName;
+			TimeBlock.Text = testObj.time;
+			QuestionsPanel.Children.Add(CreateVisualOfQuestions(testObj));
+
+			
+			//DispatcherTimer TestTimer = new DispatcherTimer();
+			//TestTimer.Tick += new EventHandler(TestTimer_Tick);
+			//TestTimer.Interval = new TimeSpan(0, 0, 1);
+			//TestTimer.Start();
+		}
+		private void TestTimer_Tick(object sender, EventArgs e)
+		{
+			// Updating the Label which displays the current second
+			//TimeBlock.Text
+
+			// Forcing the CommandManager to raise the RequerySuggested event
+			CommandManager.InvalidateRequerySuggested();
+		}
+
+		private StackPanel CreateVisualOfQuestions(Test test)
+		{
+			StackPanel questionsPanel = new();
+			foreach (Question question in test.questions)
+			{
+				QuestionVisual que = new();
+				que.QuestionName.Text = question.questionName;
+				foreach (string option in question.options)
+				{
+					RadioButton answer = new()
+					{
+						GroupName = question.questionName,
+						Content = option,
+						Margin = new Thickness(5),
+					};
+					answer.Checked += Answer_Checked;
+					que.OptionsPanel.Children.Add(answer);
+				}
+				questionsPanel.Children.Add(que);
+			}
+			return questionsPanel;
+		}
+		private void AcceptAnswers() {
+			//foreach (QuestionVisual auestion in QuestionsPanel)
+		}
+		private void Answer_Checked(object sender, RoutedEventArgs e)
+		{
+			answers.Remove(((RadioButton)sender).GroupName);
+			answers.Add(((RadioButton)sender).GroupName,((RadioButton)sender).Content.ToString());
+		}
+		private void Confirm_Click(object sender, RoutedEventArgs e)
+		{
+			AcceptAnswers();
+		}
+	}
 }
