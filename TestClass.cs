846aabfe9362ef955975e062ff3cfb926f342e6d
Максим Шабанов <maksim20048.ru@gmail.com>
2023-04-11 03:04:20 +0400

    добавил запись в файл, функция приема ответов

--- TestClass.cs
+++ TestClass.cs
@@ -1,39 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 
 
 namespace cursach
 {
-    public struct Test
-    {
-        public string testName;
-        public string time;
-        public List<Question> questions;
-        public Dictionary<int, int> marks;
-    }
-    public struct Question
-    {
-        public string questionName;
-        public List<string> options;
-        public List<string> rightAnswers;
-    }
-    class TestClass
-    {
+	public struct Test
+	{
+		public string testName;
+		public string time;
+		public List<Question> questions;
+		public Dictionary<int, int> marks;
+	}
+	public struct Question
+	{
+		public string questionName;
+		public List<string> options;
+		public List<string> rightAnswers;
+	}
+	class TestClass
+	{
+        static string path = "C:/ProgramFiles/Ubininzi";
         internal static List<Test> CreateListOfTestsFromLocalDirectory()
-        {
-            string[] ListOfPathToTests = Directory.GetFiles("C:\\Users\\Максим\\AppData\\Local\\Ubininzi", "*.json");
-            List<Test> listOfTests = new List<Test>();
-            if (ListOfPathToTests != null)
-            {
-                foreach (string testFilePath in ListOfPathToTests)
-                {
-                    string jsonText = File.ReadAllText(testFilePath);
-                    listOfTests.Add(JsonConvert.DeserializeObject<Test>(jsonText));
-                }
-            }
-            return listOfTests;
+		{
+            Directory.CreateDirectory(path);
+			if (Directory.GetFiles(path).Length != 0) {
+				string[] ListOfPathToTests = Directory.GetFiles(path,"*.json");
+				List<Test> listOfTests = new List<Test>();
+				if (ListOfPathToTests != null)
+				{
+					foreach (string testFilePath in ListOfPathToTests)
+					{
+						string jsonText = File.ReadAllText(testFilePath);
+						listOfTests.Add(JsonConvert.DeserializeObject<Test>(jsonText));
+					}
+				}
+				return listOfTests;
+			}
+            return new List<Test>();
         }
-    }
+		internal static void CreateNewRecordOfCompletedTest(string surname, string name, string group, string testName, int mark) {
+			List<string> results = File.ReadAllLines(path + "/Results.csv", System.Text.Encoding.UTF8).ToList();
+			if (!results[0].Contains(testName)) {
+				results[0] += "," + testName;
+            }
+			int IndexOfTest = Array.IndexOf(results[0].Split(','), testName);
+
+			bool IsRecordExists = false;
+			for (int i = 0; i < results.Count; i++){
+                List<string> SplittedRecord = results[i].Split(',').ToList();
+				if (SplittedRecord[0] == surname && SplittedRecord[1] == name && SplittedRecord[2] == group) {
+					for (int j = 3; j < IndexOfTest; j++){               
+						if (SplittedRecord[j]==null)
+							SplittedRecord.Add(" ");
+					}
+					SplittedRecord.Add(mark.ToString());
+                    results[i] = String.Join(',',SplittedRecord);
+					IsRecordExists = true;
+				}
+			}
+
+			if (!IsRecordExists) {
+				results.Add(surname + ',' + name + ',' + group + ',' + (new string(',', IndexOfTest - 3)) + mark.ToString());
+			}
+
+			File.WriteAllLines(path + "/Results.csv",results,System.Text.Encoding.UTF8);
+		}
+	}
 }
--- TestWindow.xaml.cs
+++ TestWindow.xaml.cs
@@ -9,12 +9,17 @@ namespace cursach
 {
 	public partial class TestWindow : Window
 	{
-		Dictionary<string, string> answers = new();
+		private Dictionary<string,string> answers = new();
+		private Dictionary<string, string> RightAnswers = new();
+		private Dictionary<int, int> marks;
+
+
 		public TestWindow(Test testObj)
 		{
 			InitializeComponent();
 			TestName.Text = testObj.testName;
 			TimeBlock.Text = testObj.time;
+			marks = testObj.marks;
 			QuestionsPanel.Children.Add(CreateVisualOfQuestions(testObj));
 
 			
@@ -23,22 +28,22 @@ namespace cursach
 			//TestTimer.Interval = new TimeSpan(0, 0, 1);
 			//TestTimer.Start();
 		}
-		private void TestTimer_Tick(object sender, EventArgs e)
-		{
-			// Updating the Label which displays the current second
-			//TimeBlock.Text
+		//private void TestTimer_Tick(object sender, EventArgs e)
+		//{
+		//	// Updating the Label which displays the current second
+		//	//TimeBlock.Text
 
-			// Forcing the CommandManager to raise the RequerySuggested event
-			CommandManager.InvalidateRequerySuggested();
-		}
+		//	// Forcing the CommandManager to raise the RequerySuggested event
+		//	CommandManager.InvalidateRequerySuggested();
+		//}
 
-		private StackPanel CreateVisualOfQuestions(Test test)
+		private StackPanel CreateVisualOfQuestions(Test test)					
 		{
 			StackPanel questionsPanel = new();
 			foreach (Question question in test.questions)
 			{
 				QuestionVisual que = new();
-				que.QuestionName.Text = question.questionName;
+				que.QuestionName.Text = question.questionName; 
 				foreach (string option in question.options)
 				{
 					RadioButton answer = new()
@@ -51,20 +56,39 @@ namespace cursach
 					que.OptionsPanel.Children.Add(answer);
 				}
 				questionsPanel.Children.Add(que);
+
+				RightAnswers.Add(question.questionName, question.rightAnswers[0]);
+
 			}
 			return questionsPanel;
 		}
-		private void AcceptAnswers() {
-			//foreach (QuestionVisual auestion in QuestionsPanel)
+		private int AcceptAnswers() {
+			int points = 0;
+			foreach (string Answer in answers.Keys)
+			{
+				if (answers[Answer] == RightAnswers[Answer]) {
+					points++;
+				}
+			}
+			foreach (int mark in marks.Keys)
+			{
+				if (points >= marks[mark])
+				{
+					MessageBox.Show(mark.ToString(),"Ваша оценка");		//ДОБАВИТЬ ЗАКРЫТИЕ ОКНА ПОСЛЕ ЗАКРЫТИЯ МЕСЕДЖБОКСА
+					return mark;
+				}
+			}
+			MessageBox.Show("2","Ваша оценка");
+			return 2;
 		}
-		private void Answer_Checked(object sender, RoutedEventArgs e)
+		private void Answer_Checked(object sender, RoutedEventArgs e)				
 		{
 			answers.Remove(((RadioButton)sender).GroupName);
 			answers.Add(((RadioButton)sender).GroupName,((RadioButton)sender).Content.ToString());
 		}
 		private void Confirm_Click(object sender, RoutedEventArgs e)
 		{
-			AcceptAnswers();
+			TestClass.CreateNewRecordOfCompletedTest(Surname.Text,Name.Text,GroupName.Text, TestName.Text, AcceptAnswers());
 		}
 	}
 }
