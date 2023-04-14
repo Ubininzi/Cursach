dbd9b62665f32c5bc294298b12efdfb58a301a5a
Максим Шабанов <maksim20048.ru@gmail.com>
2023-04-14 19:13:03 +0400

    убрал ненужные using

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
-using System.IO;
 using System.Windows.Controls;
-using System.Windows.Media;
-using cursach;
-using System.Windows.Shapes;
 
 namespace cursach
 {
